0d078c09548cd5cc44af769b8816f7bce22193b3
Bryan-Krausch <contact.krausch@gmail.krausch>
2022-08-20 15:01:17 +0200

    Legals fixed

--- src/components/modal/LegalModal.js
+++ src/components/modal/LegalModal.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {useRef} from 'react'
 import useClickOutside from "../../hook/useClickOutside"
 
-export default function LegalModal({legalModal, setLegalModal}){
+export default function LegalModal({legalModal, setLegalModal, setConfidentialModal}){
     const containerRef = useRef(null)
     useClickOutside(containerRef, legalModal, setLegalModal)
 
@@ -19,14 +19,24 @@ export default function LegalModal({legalModal, setLegalModal}){
                     <div className="p-3 md:p-0">
                         <p className="text-[17px] leading-[40px] text-dark-blue">
                         Les présentes mentions légales sont fournies en application de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique. 
-                        L’Editeur du présent site est la société STEP UP DIGITAL, Société par Action Simplifiée au capital de 1000,00 €, immatriculée au RCS de Bobigny sous le numéro 831 726 559 dont le siège social est situé au 29 rue Cartier Bresson, 93500, PANTIN et le numéro TVA FR 92831726559, représentée par Monsieur Andy TEBOUL, en sa qualité de Président. 
-                        Le site www.artisan-pac.info sous la marque Optineo est la propriété de la société STEP UP DIGITAL. 
-                        N° de téléphone de l’éditeur : 01 84 74 24 91 Le Directeur de publication du site est Monsieur Andy TEBOUL, qui pourrait être contacté à l’adresse contact@stepup-digital.com
-                        le présent site web est hébergé par la société OVH dont le siège est situé à l’adresse ci-après: 2 Rue Kellermann, 59100 Roubaix, France. 
-                        L’hébergeur peut être contacté au numéro de téléphone suivant : 1007 L’adresse email de l’hébergeur est support@ovh.fr 
-                        Un délégué à la protection des données est à votre disposition pour toute question relative à la protection de vos données personnelles. 
-                        Vous pouvez lui écrire à : -DPO STEP UP DIGITAL – 29 rue Cartier Bresson – 93500, PANTIN. -dpo@stepup-digital.com 
-                        La politique de confidentialité applicable au site est accessible à l’adresse 
+                        L’Editeur du présent site est la société STEP UP DIGITAL, Société par Action Simplifiée au capital de 1000,00 €, immatriculée au RCS de Bobigny 
+                        sous le numéro 831 726 559 dont le siège social est situé au 29 rue Cartier Bresson, 93500, PANTIN et le numéro TVA FR 92831726559,
+                         représentée par Monsieur Andy TEBOUL, en sa qualité de Président. Le site www.pac.optineo.info sous la marque Optineo est la propriété de la société STEP UP DIGITAL. 
+                         N° de téléphone de l’éditeur : 01 84 74 24 90 Le Directeur de publication du site est Monsieur Andy TEBOUL, qui pourrait être contacté à l’adresse contact@stepup-digital.com 
+                         le présent site web est hébergé par la société OVH dont le siège est situé à l’adresse ci-après: 2 Rue Kellermann, 59100 Roubaix, France. 
+                         L’hébergeur peut être contacté au numéro de téléphone suivant : 1007 L’adresse email de l’hébergeur est support@ovh.fr 
+                         Un délégué à la protection des données est à votre disposition pour toute question relative à la protection de vos données personnelles. 
+                         Vous pouvez lui écrire à : -DPO STEP UP DIGITAL – 29 rue Cartier Bresson – 93500, PANTIN. -dpo@stepup-digital.com La politique de confidentialité applicable au site est accessible 
+                         <button 
+                            className="text-blue-800 text-[25px] pl-2"
+                            onClick={() => {
+                                setLegalModal(false)
+                                setConfidentialModal(true)
+                            }}
+                        > 
+                         ici
+                        </button>
+
                         </p>
                     </div>
                 </div>
--- src/components/modal/ModalContainer.js
+++ src/components/modal/ModalContainer.js
@@ -7,7 +7,7 @@ import LegalModal from "./LegalModal";
 export default function ModalContainer({legalModal, setLegalModal, confidentialModal, setConfidentialModal, cgu, setCgu}){
     return(
         <div className="modal-container">
-            {legalModal && <LegalModal legalModal={legalModal} setLegalModal={setLegalModal}/>}
+            {legalModal && <LegalModal legalModal={legalModal} setLegalModal={setLegalModal} setConfidentialModal={setConfidentialModal}/>}
             {confidentialModal && <ConfidentialModal confidentialModal={confidentialModal} setConfidentialModal={setConfidentialModal}/>}
             {cgu && <CguModal cgu={cgu} setCgu={setCgu} setConfidentialModal={setConfidentialModal}/>}
         </div>
