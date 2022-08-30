78c8a496f8b12cf6141b100dcfaf19151e26ce23
Bryan-Krausch <contact.krausch@gmail.com>
2022-08-30 13:18:46 +0200

    Meta

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import HttpsRedirect from 'react-https-redirect'
+import DocumentMeta from 'react-document-meta';
 
+const meta = {
+  title: 'Pourquoi installer une pompe à chaleur ?',
+  description: "Pourquoi installer une pompe à chaleur ? Gratuit et sans engament, 70% d'économie sur vos factures, Jusqu'a 10 500€ d'aide pour votre pompe à chaleur, Jusqu'a 100% de financement",
+  meta: {
+    charset: 'utf-8',
+    name: {
+      keywords: 'optineo, pac, pompe-a-chaleur, pac-info'
+    }
+  }
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HttpsRedirect>
-      <App />
-    </HttpsRedirect>
+    <DocumentMeta {...meta}>
+      <HttpsRedirect>
+        <App />
+      </HttpsRedirect>
+    </DocumentMeta>
   </React.StrictMode>
 );
