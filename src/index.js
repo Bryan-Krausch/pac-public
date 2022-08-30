c5a056a7532a016305e072c1c928cbe0d2c0cb31
Bryan-Krausch <contact.krausch@gmail.com>
2022-08-30 13:36:09 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import HttpsRedirect from 'react-https-redirect'
-import {Helmet} from "react-helmet";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <HttpsRedirect>
-        <Helmet>
-          <meta name="description" content="Pourquoi installer une pompe à chaleur ? Gratuit et sans engament, 70% d'économie sur vos factures, Jusqu'a 10 500€ d'aide pour votre pompe à chaleur, Jusqu'a 100% de financement"/>
-        </Helmet>
         <App />
       </HttpsRedirect>
   </React.StrictMode>
