ebe8ad79818983c31c492e83cd603e71f2c22120
Bryan-Krausch <contact.krausch@gmail.com>
2022-09-01 11:59:00 +0200

    icon fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,6 +7,7 @@ import { faCheck, faClipboardCheck } from "@fortawesome/free-solid-svg-icons"
 import axios from 'axios'
 import {ToastContainer, toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import ReactGA from 'react-ga';
 
 import firstImage from "../assets/brandLogo/1.png"
 import secondImage from "../assets/brandLogo/prime.png"
@@ -95,7 +96,10 @@ export default function Hero({routes}){
                 type_chauffage: heatingType
             }).then((response) => {
                 if(response.status === 200){
-                    window.dataLayer.push({"event":  "submitForm"})
+                    // ReactGA.event({
+                    //     category: "test",
+                    //     action: ""
+                    // })
                      toast.success("Formulaire envoyé avec succès")
                 }
             }).catch((error) => {
--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import './index.css';
 import App from './App';
 import HttpsRedirect from 'react-https-redirect'
 import TagManager from "react-gtm-module"
+ReactGA.initialize('GTM-5S3B86F');
 
-const TagManagerArgs = {
-  gtmId: "GTM-5S3B86F"
-}
-
-TagManager.initialize(TagManagerArgs)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
