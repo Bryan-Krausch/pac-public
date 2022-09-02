3aa1de59b4dd679829d5868b2345590dfd99ff5b
Bryan-Krausch <contact.krausch@gmail.com>
2022-09-02 10:22:02 +0200

    ...

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
         <header className="flex flex-col items-center w-full text-[16px] antialiased ">
             <div className='p-2 w-full flex flex-col justify-center gap-y-[40px] md:mx-auto md:max-w-[1140px] md:flex-row md:items-center md:justify-center'>
                 <div className='flex w-full'>
-                    <a href="https://pac.optineo.info/" className="w-full flex justify-center md:justify-start">
+                    <a href="https://pac.optineo.info/" className="flex justify-center md:justify-start">
                         <img width={345} height={205} alt="logo optineo" src={headerIconeImage} className="w-[130px] h-[78px] img-header align-middle" />
                     </a>
                 </div>
