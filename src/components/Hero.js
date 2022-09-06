fbb7b0318eb75737c081c9ee94b2ffcc29f2bdc9
Bryan-Krausch <contact.krausch@gmail.com>
2022-09-06 15:47:22 +0200

    ...

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -99,9 +99,9 @@ export default function Hero({routes}){
             }).then((response) => {
                 console.log(response)
                 if(response.status === 200){
-                    axios.get(`https://tracker.sud-plateforme.fr/?c=3N6HQ5ZTG7&l[t]=PAC3&l[rid]=${response.data.success.insertId}&l[e]=${email}L&u=${response.data.success.insertId}`, {
+                    axios.get(`https://api.pac.optineo.info/tracking/${email}/${response.data.success.insertId}`, {
                         headers: {
-                            "Access-Control-Allow-Origin": 'https://pac.optineo.info/'
+                            "Access-Control-Allow-Origin": '*'
                         }
                     }).then((res) => {
                         console.log(res)
